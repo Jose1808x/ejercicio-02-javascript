685df920ebdb2d205ccaea30f21a4026df53008e
jose <josearodriguez3954@gmail.com>
2023-01-02 23:49:28 -0400

    Este es mi ejercicio numero 2 en javascript

---
+++ ejercicio02.js
@@ -0,0 +1,13 @@
+/*Mi breve descripcion en el ejercicio numero 2 de Javascript en el curso de Open Bootcamp*/
+
+let nombre = "Jose Rodriguez";
+let edad = 30;
+let dev = true;
+let nacimiento = new Date(1992, 07, 18);
+
+const libro = {
+    titulo: "Game Of Thrones",
+    autor: "George R.R. Martin",
+    fecha: new Date(2019, 03, 09),
+    url: "https://dariososafoula.files.wordpress.com/2017/01/cancion-de-hielo-y-fuego-1-juego-de-tronos.pdf"
+}
